f5bac543e361a74be30e1f0e31ff7c6135889cbb
nathansearles <nsearles@gmail.com>
2019-10-18 15:29:03 -0700

    added meta description

--- components/Head.js
+++ components/Head.js
@@ -10,13 +10,13 @@ const Title = ({ title }) => {
         content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=10.0,minimal-ui"
       />
       <link rel="icon" href="/static/favicon.ico" />
-
+      <meta name="Description" content="Web developer focused on technology that drives great user experience and design." />
       <meta
         name="title"
         property="og:title"
         content={`${title} | Nathan Searles`}
-      ></meta>
-      <meta name="author" content="Nathan Searles"></meta>
+      />
+      <meta name="author" content="Nathan Searles" />
     </Head>
   );
 };
