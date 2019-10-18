893cbdab5ce6b0e410afa3ca06023b0a3dae362b
nathansearles <nsearles@gmail.com>
2019-10-18 14:53:08 -0700

    moved theme to a state

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,30 @@ import { AnimatePresence } from "framer-motion";
 import ScrollController from "../components/ScrollControlller";
 
 class Site extends App {
+  constructor() {
+    super();
+    this.state = { theme: "light" };
+  }
+
   componentDidMount() {
     if ("scrollRestoration" in history) {
       // Handle scroll with ScrollController
       history.scrollRestoration = "manual";
     }
-    const colorScheme = window.matchMedia("(prefers-color-scheme: dark)")
-      .matches;
+
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      this.setState({ theme: "dark" });
+    }
   }
   render() {
     const { Component, pageProps, router } = this.props;
 
     return (
       <ScrollController>
-        <div
-          className={`theme ${
-            this.colorScheme ? "theme-dark" : "theme--default"
-          }`}
-        >
+        <div className={`theme theme--${this.state.theme}`}>
           <AnimatePresence exitBeforeEnter initial={false}>
             <Component {...pageProps} key={router.route} />
           </AnimatePresence>
