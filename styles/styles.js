09f9d31068d1b14df47e5d8cc3b7b01b5dce70be
nathansearles <nsearles@gmail.com>
2019-10-22 11:13:50 -0700

    test of imported base styled jsx

--- styles/styles.js
+++ styles/styles.js
@@ -1,5 +1,6 @@
 /* styles.js */
 import css from "styled-jsx/css";
+import base from "./base.js";
 
 // Scoped styles
 export const button = css`
@@ -9,80 +10,7 @@ export const button = css`
 `;
 
 // Global styles
-export const body = css.global`
-  // Inter: https://rsms.me/inter/
-  @font-face {
-    font-family: "Inter";
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url("/static/fonts/Inter-Regular.woff2") format("woff2"),
-      url("/static/fonts/Inter-Regular.woff") format("woff");
-  }
-
-  @font-face {
-    font-family: "Inter";
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: url("/static/fonts/Inter-SemiBold.woff2") format("woff2"),
-      url("/static/fonts/Inter-SemiBold.woff") format("woff");
-  }
-
-  @font-face {
-    font-family: "Inter";
-    font-style: normal;
-    font-weight: 700;
-    font-display: swap;
-    src: url("/static/fonts/Inter-Bold.woff2") format("woff2"),
-      url("/static/fonts/Inter-Bold.woff") format("woff");
-  }
-
-  @mixin regular-font() {
-    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-      Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    font-style: normal;
-    font-weight: 400;
-  }
-
-  @mixin semibold-font() {
-    font-family: "Inter", sans-serif;
-    font-style: normal;
-    font-weight: 600;
-  }
-
-  @mixin bold-font() {
-    font-family: "Inter", sans-serif;
-    font-style: normal;
-    font-weight: 700;
-  }
-
-  html {
-    font-size: 18px;
-    line-height: 30px;
-    -webkit-text-size-adjust: 100%;
-    /* Font varient */
-    font-variant-ligatures: none;
-    -webkit-font-variant-ligatures: none;
-    /* Smoothing */
-    text-rendering: optimizeLegibility;
-    -moz-osx-font-smoothing: grayscale;
-    font-smoothing: antialiased;
-    -webkit-font-smoothing: antialiased;
-    text-shadow: rgba(0, 0, 0, 0.01) 0 0 1px;
-  }
-
-  body {
-    @include regular-font;
-    margin: 0;
-    font-size: 1rem;
-    line-height: 1.5rem;
-    letter-spacing: -0.011em;
-
-    /* Disable tap highlight */
-    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
-  }
-`;
+export const body = base;
 
 // Resolved styles
 export const link = css.resolve`
