111b0a8d8d03717c28df3b360b44b1eb250845e5
Nathan Searles <nsearles@gmail.com>
2021-11-14 10:25:01 -0800

    moved footer to homepage

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,7 @@
-import Footer from "@components/Footer";
-
 export default function Layout({ children }) {
   return (
     <>
       {children}
-      <Footer />
       <div hidden>
         <span id="new-window-0">Opens in a new window</span>
         <span id="new-window-1">Opens an external site</span>
