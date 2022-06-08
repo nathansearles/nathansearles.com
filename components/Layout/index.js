2718880e1a7e22b0daf39372dcd69bf3260aef35
Nathan Searles <nsearles@gmail.com>
2022-06-07 20:53:36 -0700

    added lenis smooth scrolling

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   useEffect(() => {
     const splashTimer = setTimeout(() => {
       setSplashActive(false);
-    }, 2150);
+    }, 2200);
     return () => {
       clearTimeout(splashTimer);
     };
--- components/Splash/index.js
+++ components/Splash/index.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import styles from "./splash.module.scss";
 
 export default function Splash() {
-  const splashVariants = {
+  const containerVariants = {
     initial: {
       opacity: 1,
     },
@@ -16,7 +16,7 @@ export default function Splash() {
       transition: { type: "spring", damping: 80, stiffness: 400, delay: 0.35 },
     },
   };
-  const logoVariants = {
+  const textVariants = {
     initial: {
       scale: 1.25,
       opacity: 0,
@@ -24,7 +24,7 @@ export default function Splash() {
     enter: {
       scale: 1,
       opacity: 1,
-      transition: { type: "spring", damping: 80, stiffness: 400, delay: 0.75 },
+      transition: { type: "spring", damping: 80, stiffness: 400, delay: 0.5 },
     },
     exit: {
       scale: 0.85,
@@ -39,12 +39,12 @@ export default function Splash() {
     <FocusOn>
       <motion.div
         className={styles.splash}
-        variants={splashVariants}
+        variants={containerVariants}
         initial="initial"
         animate="enter"
         exit="exit"
       >
-        <motion.div className={styles.text} variants={logoVariants}>
+        <motion.div className={styles.text} variants={textVariants}>
           Nathan Searles
         </motion.div>
       </motion.div>
