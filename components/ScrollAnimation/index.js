aff57336aedb8cacb231fc8d72fa02a329af2a61
Nathan Searles <nsearles@gmail.com>
2022-06-08 20:55:39 -0700

    updated scroll animation use scrub

--- components/ScrollAnimation/index.js
+++ components/ScrollAnimation/index.js
@@ -1,26 +1,33 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { useWindowDimensions } from "@hooks/useWindowDimensions";
+import { useRect } from "@hooks/useRect";
+
+const options = {
+  viewport: [1, 0.5], // [start, end] a number between 0 and 1 indicating the percentage where in the viewport the animation should start and end.
+  element: [0, 0], // [start, end] a number between 0 and 1 indicating the element percentage the animation should start and end.
+};
 
 export default function ScrollAnimation({ children }) {
-  const variants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        ease: "easeOut",
-        duration: 1.25,
-      },
-    },
-  };
+  const ref = useRef();
+  const { height: windowHeight } = useWindowDimensions();
+  const { top, bottom, height } = useRect(ref);
+
+  const [viewportStart, viewportEnd] = options.viewport;
+  const [elementStart, elementEnd] = options.element;
+
+  const { scrollY } = useViewportScroll();
+
+  const primaryMotionValue = [
+    top + height * elementStart - windowHeight * viewportStart,
+    bottom - height * elementEnd - windowHeight * viewportEnd,
+  ];
+
+  const opacity = useTransform(scrollY, [...primaryMotionValue], [0, 1]);
+
   return (
-    <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: "some" }}
-      variants={variants}
-    >
+    <motion.span ref={ref} style={{ opacity }}>
       {children}
-    </motion.div>
+    </motion.span>
   );
 }
---
+++ hooks/useWindowDimensions.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowDimensions;
+};
