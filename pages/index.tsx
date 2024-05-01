a53c23c6223fbbaae90e703df0f779513f97b2f9
Nathan Searles <nsearles@gmail.com>
2024-05-01 15:50:25 -0700

    copy updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,23 +23,28 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className="visually-hidden">Nathan Searles</h1>
         <p>
-          Front-end engineer with a focus on design, user experience and
-          accessibility.
+          I specialize in front-end engineering, placing a strong emphasis on
+          design principles, user experience, and accessibility. My passion lies
+          in creating visually appealing and user-friendly interfaces that are
+          accessible to everyone.
         </p>
         <p>
-          I&apos;m the Technical Director at{" "}
-          <a href="https://studiomega.com/">Studio Mega</a> where I lead all
-          things digital.
+          As the Technical Director at{" "}
+          <a href="https://studiomega.com/">Studio Mega</a>, I lead our digital
+          initiatives, guiding and overseeing all aspects of our technical
+          projects.
         </p>
         <h2 className={styles.label}>Previous Experience</h2>
         <section className={styles.marginBottom}>
           <h3>Cinco</h3>
           <h4>Technical Director</h4>
           <p>
-            Supported digital projects across a design-led studio, from
-            proposals and SOWs, to project planning, strategy and hands on
-            development. This included complete website rebuilds to working with
-            existing code bases and platforms. Clients included The James Brand,
+            Orchestrated diverse digital initiatives within a design-focused
+            studio, adeptly navigating from initial proposals and SOWs to
+            meticulous project planning, strategic implementation, and hands-on
+            development. Led comprehensive website overhauls while seamlessly
+            collaborating with established code bases and platforms. Notable
+            clientele encompassed industry leaders such as The James Brand,
             Nike, Gogoro, and Nextbit.
           </p>
         </section>
@@ -47,35 +52,42 @@ export default function Home() {
           <h3>Fuzzco</h3>
           <h4>Director of Technology</h4>
           <p>
-            Managed team of developers and established a technology process.
-            Oversaw a roster of existing WordPress websites, supported new
-            business, day-to-day development and project/account management of
-            new and existing clients. Worked with Obey, Hugh &amp; Crye and a
-            range of small to medium-sized businesses.
+            Led a dynamic team of developers and spearheaded the implementation
+            of streamlined technology processes. Managed a portfolio of
+            WordPress websites, providing comprehensive support for both
+            established clients and new business ventures. Juggled the demands
+            of daily development tasks and project/account management with
+            finesse, catering to a diverse clientele ranging from industry
+            giants like Obey and Hugh & Crye to a variety of small to
+            medium-sized businesses.
           </p>
         </section>
         <section>
           <h3>The Brigade</h3>
           <h4>Partner &amp; Director of Technology</h4>
           <p>
-            Co-founded and helped run all aspects of a newly-formed digital
-            agency. Oversaw new business, project, account, and team management,
-            to hands-on development for clients including Spotify, Intel, and
-            Mindjet.
+            Played a pivotal role as co-founder in launching and managing a
+            cutting-edge digital agency from the ground up. From steering new
+            business ventures to orchestrating project and account management,
+            as well as nurturing team dynamics, I had a hand in every aspect.
+            And yes, I also rolled up my sleeves for hands-on development work,
+            contributing to successful projects for esteemed clients such as
+            Spotify, Intel, and Mindjet.
           </p>
         </section>
         <h2 className={styles.label}>Technology</h2>
         <section>
           <p>
-            Working on the <strong>Jamstack</strong> to build responsive
-            interfaces using the latest in <strong>HTML</strong>,{" "}
-            <strong>CSS</strong> and <strong>JavaScript</strong>.
-          </p>
-          <p>
-            Using modern front-end libraries and frameworks like{" "}
-            <strong>React</strong> and <strong>Next.js</strong>, versioned with{" "}
-            <strong>Git</strong> and connected to a{" "}
-            <strong>headless CMS</strong> or custom-built <strong>API</strong>.
+            I specialize in crafting responsive interfaces using the Jamstack
+            architecture, powered by the latest advancements in{" "}
+            <strong>HTML</strong>, <strong>CSS</strong>, and
+            <strong>JavaScript</strong>. By utilizing contemporary front-end
+            libraries like <strong>React</strong>
+            and frameworks such as <strong>Next.js</strong>, I ensure robust
+            development practices. Each project is meticulously versioned with{" "}
+            <strong>Git</strong> and seamlessly connected to either a{" "}
+            <strong>headless CMS</strong> or a bespoke <strong>API</strong>,
+            delivering unparalleled user experiences.
           </p>
         </section>
       </main>
