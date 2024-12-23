e74e743e1a34194f76b407a63f1dfc7f075ea19d
Nathan Searles <nsearles@gmail.com>
2024-12-23 13:00:16 -0800

    Added tailwind pointer fine and course

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,8 +9,14 @@ export default {
       backgroundColorMuted: "var(--background-color-muted)",
       foregroundColor: "var(--foreground-color)",
       foregroundColorMuted: "var(--foreground-color-muted)",
+      yellow: "var(--yellow)",
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("pointer-coarse", "@media (pointer: coarse)");
+      addVariant("pointer-fine", "@media (pointer: fine)");
+    }),
+  ],
 };
