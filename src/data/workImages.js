36306f8f02ff1ae5574c8045fdfcf40c29264ae8
Nathan Searles <nsearles@gmail.com>
2025-07-01 10:16:05 -0700

    Update work images and layout

---
+++ src/data/workImages.js
@@ -0,0 +1,90 @@
+export const workImages = [
+  {
+    src: "izze-product.png",
+    alt: "Izze: Product",
+  },
+  {
+    src: "tropicana-products-featured.png",
+    alt: "Tropicana: Featured Products",
+  },
+  {
+    src: "driftwell-homepage-hero.png",
+    alt: "Driftwell: Homepage Hero",
+  },
+  {
+    src: "monument-home-hero.png",
+    alt: "Monument: Home Hero",
+  },
+  {
+    src: "soulboost-products.png",
+    alt: "Soulboost: Products",
+  },
+  {
+    src: "driftwell-homepage-module.png",
+    alt: "Driftwell: Homepage Module",
+  },
+  {
+    src: "umuddled-products.png",
+    alt: "Umudled: Products",
+  },
+  {
+    src: "neon-zebra-carousel.png",
+    alt: "Neon Zebra: Carousel",
+  },
+  {
+    src: "studio-mega-projects.png",
+    alt: "Studio Mega: Projects",
+  },
+  {
+    src: "studio-mega-team.png",
+    alt: "Studio Mega: Team",
+  },
+  {
+    src: "izze-social.png",
+    alt: "Izze: Social",
+  },
+  {
+    src: "kodiak-video-scroll-2.png",
+    alt: "Kodiak: Video Scroll",
+  },
+  {
+    src: "powder-mountain-home-hero.png",
+    alt: "Powder Mountain: Home Hero",
+  },
+  {
+    src: "beatons.png",
+    alt: "Beatons: Homepage",
+  },
+  {
+    src: "powder-haven-real-estate-listing.png",
+    alt: "Powder Haven: Real Estate Listing",
+  },
+  {
+    src: "izze-products.png",
+    alt: "Izze: Products",
+  },
+  {
+    src: "neon-zebra-age-gate.png",
+    alt: "Neon Zebra: Age Gate",
+  },
+  {
+    src: "studio-mega-team.png",
+    alt: "Studio Mega: Team",
+  },
+  {
+    src: "starry-home.png",
+    alt: "Starry: Home",
+  },
+  {
+    src: "powder-haven-home-hero.png",
+    alt: "Powder Haven: Home Hero",
+  },
+  {
+    src: "umuddled-cocktail.png",
+    alt: "Umuddled: Cocktail",
+  },
+  {
+    src: "monument-home-module.png",
+    alt: "Monument: Home Module",
+  },
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function getRandomImages(
-  array: { src: string; alt: string }[],
-  count: number,
-) {
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled.slice(0, count);
-}
