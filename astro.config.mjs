d74431b22bdb56c4685f5bf2a087158cc980c0e8
Nathan Searles <nsearles@gmail.com>
2025-07-07 15:38:39 -0700

    Refactor analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: vercel(),
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
